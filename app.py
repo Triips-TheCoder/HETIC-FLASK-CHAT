8988669777510e2e987d0cbe258d0cd30213cdca
NicoooM <nmarsan33000@gmail.com>
2023-12-01 11:16:40 +0100

    feat: begin flask config

--- app.py
+++ app.py
@@ -1,6 +1,21 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from db import SQLALCHEMY_DATABASE_URI
 
-app = Flask(__name__, template_folder='templates', static_folder='static')
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URI
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+with app.app_context():
+    db.create_all()
 
 @app.route('/')
 def index():
