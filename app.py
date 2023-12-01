912606312d11247e6555d951e032f233455544e0
Lucas Boucher <contact@lucasboucher.fr>
2023-12-01 09:57:35 +0100

    first template

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
-app = Flask(__name__)
-
+app = Flask(__name__, template_folder='templates', static_folder='static')
 
 @app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/api')
 def hello_world():  # put application's code here
     return 'Hello World!'
 
-
-if __name__ == '__main__':
-    app.run()
+if __name__=='__main__':
+    app.run(debug = True)
